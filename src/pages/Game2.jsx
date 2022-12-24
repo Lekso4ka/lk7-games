82855a7699015a2710e980d96338fdb4afb698ef
Leksa <admin@petiteweb.ru>
2022-12-24 22:14:22 +0300

    upd game

--- src/pages/Game2.jsx
+++ src/pages/Game2.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import SuCard from "../components/SuCard";
 export default () => {
     const n = 3;
+    const data = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--";
     const [board, setBoard] = useState([]);
     useEffect(() => {
         let cnt = n ** 2;
@@ -10,11 +11,11 @@ export default () => {
             brd.push([]);
             for (let j = 0; j < cnt; j++) { 
                 //console.log(i, j, i * cnt + j);
-                brd[i].push(null);
+                brd[i].push(data[i * cnt + j] !== "-" ? data[i * cnt + j] : null);
                 /*
                     i = 1, j = 0-8, cnt = 9
                     0 2 4 6 8 10 12 14 16
-                    9 10 11 12 13 14 15 16 17                
+                    9 10 11 12 13 14 15 16 17          
                 */
             }
         }
@@ -27,9 +28,16 @@ export default () => {
             gridTemplateColumns: `repeat(${n**2}, auto)`
         }}
     >
-        {board.reduce((arr, el, i) => {
+        {board.length > 0 && board.reduce((arr, el, i) => {
             el.forEach((cell, j) => {
-                arr.push(<SuCard key={i * (n**2) + j} n={i * (n**2) + j} content={cell} cnt={n}/>)
+                arr.push(<SuCard 
+                    key={i * (n**2) + j} 
+                    n={i * (n**2) + j} 
+                    content={cell} 
+                    cnt={n}
+                    setBoard={setBoard}
+                    board={board}
+                />)
             })
             return arr;
         }, [])}
